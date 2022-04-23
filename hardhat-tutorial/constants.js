2d8f2115d70d85853378e1cf1cebd4f9e567f38d
jonathan sanfer <jonathan.sanfer@protonmail.com>
2022-04-23 10:48:15 +0200

    initial commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = '0xF0b30F52d58a01ab2F115b8B7Da06F479b07c44B';
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
